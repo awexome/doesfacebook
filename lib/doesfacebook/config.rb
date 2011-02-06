1f8ce760279ee293080748246aa622acd9f5c9c1
Colin McCloskey <colin.mccloskey@gmail.com>
2011-02-06 14:16:52 -0500

    use properly named config file from app config

--- lib/doesfacebook/config.rb
+++ lib/doesfacebook/config.rb
@@ -13,7 +13,7 @@ module DoesFacebook
     
     # Load configuration from YAML file for all environments:
     def all_facebook_config
-      config_file = File.join(Rails.root, "config", "dofacebook.yml")
+      config_file = File.join(Rails.root, "config", "doesfacebook.yml")
       if File.exist?(config_file)
         return @@all_facebook_config ||= YAML.load(File.open( config_file ))
       end
