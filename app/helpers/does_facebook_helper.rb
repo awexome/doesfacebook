1d09e28fe715e1a094f0eab9fa258c67cadcef6f
Colin McCloskey <colin.mccloskey@gmail.com>
2011-02-06 22:19:06 -0500

    swap helpers content into the helpers module

--- app/helpers/does_facebook_helper.rb
+++ app/helpers/does_facebook_helper.rb
@@ -7,12 +7,21 @@ module DoesFacebookHelper
   
   # Generate a URL that points within the Facebook canvas
   def url_for_canvas(url_opts={})
-    controller.send(:canvas_url_for, options)
+    canvas_name = controller.send(:facebook_config)["canvas_name"]
+    canvas_root = File.join("http://apps.facebook.com", canvas_name)
+    if url_opts.is_a?(Hash)
+      return File.join(canvas_root, url_for(url_opts))
+    elsif url_opts.include?("://")
+      return url_opts
+    else
+      return File.join(canvas_root, url_opts)
+    end
   end
-  
+
   # Generate a link that stays within the Facebook canvas
   def link_to_canvas(text, url_opts={}, html_opts={})
-    controller.send(:link_to_canvas, text, url_opts, html_opts)
+    url = url_for_canvas(url_opts)
+    content_tag("a", text, html_opts.merge(:rel=>"canvas", :href=>url, :target=>"_top"), false)
   end
   
 end
--- lib/doesfacebook/helpers.rb
+++ lib/doesfacebook/helpers.rb
@@ -6,22 +6,5 @@
 module DoesFacebook
   module Helpers
     
-    # Generate a URL that points within the Facebook canvas
-    def url_for_canvas(url_opts={})
-      canvas_root = File.join("http://apps.facebook.com", facebook_config[:canvas_name])
-      if url_opts.is_a?(Hash)
-        return File.join(canvas_root, url_for(url_opts))
-      elsif url_opts.include?("://")
-        return url_opts
-      else
-        return File.join(canvas_root, url_opts)
-      end
-    end
-
-    # Generate a link that stays within the Facebook canvas
-    def link_to_canvas(text, url_opts={}, html_opts={})
-      link_to(text, url_for_canavs(url_opts), html_opts.merge(:target=>"_top"))
-    end
-    
   end # Helpers
 end # DoesFacebook
