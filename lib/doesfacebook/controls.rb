659271cf2025c1ddee16f6fa97b9ba4bc855d1db
Colin McCloskey <colin.mccloskey@gmail.com>
2011-09-01 00:45:38 -0400

    JavaScript-based redirection with optional loading partial

--- lib/doesfacebook/controls.rb
+++ lib/doesfacebook/controls.rb
@@ -13,11 +13,30 @@ module DoesFacebook
     protected
     
     # Mechanism for redirecting within the Facebook canvas:
-    def redirect_to_canvas(opts={})
+    def redirect_to_canvas_old(opts={})
       raise MalformedUrlOptions.new("Options passed to `redirect_to_canvas` must be a URL options Hash") unless opts.is_a?(Hash)
       dest = File.join("http://apps.facebook.com/", facebook_config[:canvas_name], url_for(opts.merge(:only_path=>true))) 
       logger.info "Canvas Redirect to #{opts.inspect}=>#{dest}"
       redirect_to dest
+    end
+    
+    # Purer, JavaScript-based solution for redirecting within the Facebook canvas:
+    def redirect_to_canvas(opts={})
+      raise MalformedUrlOptions.new("Options passed to `redirect_to_canvas` must be a URL options Hash") unless opts.is_a?(Hash)
+      dest = File.join("http://apps.facebook.com/", facebook_config[:canvas_name], url_for(opts.merge(:only_path=>true))) 
+      @facebook_redirect_url = dest
+      logger.info "Canvas Redirect to #{opts.inspect}=>#{dest}"
+      begin
+        render :partial=>"facebook/redirect"
+      rescue ActionView::MissingTemplate=>e
+        logger.info "Pretty Redirect Partial facebook/redirect Not Found. Using Unformatted Text."
+        render :text=>%{
+          Redirecting you to <a href="#{dest}">#{dest}</a>...
+          <script type="text/javascript">
+            top.location.href = "#{dest}";
+          </script>
+        }
+      end
     end
     
   end # Controls
