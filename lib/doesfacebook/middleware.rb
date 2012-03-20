3be9f1775cb26ed9080f3c3a9fe845f793412034
Colin McCloskey <colin.mccloskey@gmail.com>
2012-03-20 17:46:32 -0400

    XHR requests should not be converted to POST

--- lib/doesfacebook/middleware.rb
+++ lib/doesfacebook/middleware.rb
@@ -12,7 +12,7 @@ module DoesFacebook
     
     def call(env)
       request = Rack::Request.new(env)
-      if request.POST["signed_request"]
+      if !request.xhr? && request.POST["signed_request"]
         env["REQUEST_METHOD"] = "GET"
         Rails.logger.info("  Facebook POST request converted to GET request")
       end
