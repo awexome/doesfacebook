ec726d0e3218e40af2725cc3faee41dfe2f96d1b
Colin McCloskey <colin.mccloskey@gmail.com>
2012-04-30 18:53:56 -0400

    fix issue where no referer crashes middleware; bump to version 0.5.4

--- lib/doesfacebook/middleware.rb
+++ lib/doesfacebook/middleware.rb
@@ -12,9 +12,14 @@ module DoesFacebook
     
     def call(env)
       request = Rack::Request.new(env)
-      if request.referer.match(/facebook\.com/) && request.POST["signed_request"]
-        env["REQUEST_METHOD"] = "GET"
-        Rails.logger.info("  Facebook POST request from #{request.referer} converted to GET request")
+      begin
+        if request.referer && request.referer.match(/facebook\.com/) && request.POST["signed_request"]
+          env["REQUEST_METHOD"] = "GET"
+          Rails.logger.info("  Facebook POST request from #{request.referer} converted to GET request")
+        end
+      rescue Exception=>e
+        Rails.logger.info("  Error in DoesFacebook request conversion: #{e.message}")
+        raise e
       end
       @app.call(env)
     end
