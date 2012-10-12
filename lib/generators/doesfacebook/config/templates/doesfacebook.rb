893fc8684c2d8fb629786fd52ae35792c86bde73
Colin McCloskey <colin.mccloskey@gmail.com>
2012-10-12 03:02:29 -0400

    fixes for initializer template

--- lib/generators/doesfacebook/config/templates/doesfacebook.rb
+++ lib/generators/doesfacebook/config/templates/doesfacebook.rb
@@ -14,17 +14,15 @@ DoesFacebook.configure do |config|
   # at developers.facebook.com) here, like so:
   config.add_application(
     id: 123456789012345,
-    secret: "993e9130d9133a883914562d75ab4557",
     secret: "abcdefghijklmnopqrstuvwxyzABCDEF"
     namespace: "your_app_namespace",
     canvas_url: "http://your.dev.server.com/and/path",
     secure_canvas_url: "https://secure.dev.server"
   )
 
-  # If you have multiple applications defined, you can specify the method
-  # by which DoesFacebook selects the appropriate configuration for each
-  # request with a lambda or Proc. This example uses canvas_url to make
-  # a distinction, but you can make this distinction however you wish:
+  # You may specify the method by which DoesFacebook selects the app
+  # configuration for each request with a lambda or Proc. This example 
+  # uses canvas_url, but you can make this distinction however you wish:
   # config.app_selector = Proc.new() do |request, apps|
   #   apps.find do |a|
   #     callback_path = request.ssl? && a.supports_ssl? ? a.secure_canvas_url : a.canvas_url
