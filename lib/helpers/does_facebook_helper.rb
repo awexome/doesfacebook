5570f4abf54b55a8ae99399e171b14d6f0192fd4
Colin McCloskey <colin.mccloskey@gmail.com>
2012-10-15 18:15:34 -0400

    helper tweaks

--- lib/helpers/does_facebook_helper.rb
+++ lib/helpers/does_facebook_helper.rb
@@ -5,14 +5,19 @@
 
 module DoesFacebookHelper
   
+  # Return a reference to the current Facebook Application config:
+  def fb_app
+    @fb_app
+  end
+
   # Return the current application id to the view
   def app_id
-    controller.send(:facebook_config)[:app_id]
+    fb_app.id
   end
   
   # Return the current app namespace:
   def app_namespace
-    controller.send(:facebook_config)[:namespace]
+    fb_app.namespace
   end
   
   # Return the full canvas URL:
@@ -28,6 +33,13 @@ module DoesFacebookHelper
   # Generate a link that points within the Facebook canvas
   def link_to_canvas(text, url_opts={}, html_opts={})
     content_tag("a", text, html_opts.merge(:rev=>"canvas", :href=>url_for_canvas(url_opts), :target=>"_top"), false)
+  end
+
+  # Display a user's Facebook profile photo
+  def profile_pic(facebook_id, opts={})
+    type = opts.delete(:type) || "square" # <= Valid types: square, small, normal, large
+    opts = {:class=>"profile-pic", :border=>0}.merge(opts)
+    image_tag("//graph.facebook.com/#{facebook_id}/picture?type=#{type}", opts)
   end
   
   # Insert properly-formed FB.init() call with fb-root doc element
