fa9d09cdd655d5d2b1b99f8a5eee0ebef06f6acb
Colin McCloskey <colin.mccloskey@gmail.com>
2013-05-21 16:36:44 -0400

    allow filters to be prepended with typical options

--- lib/doesfacebook.rb
+++ lib/doesfacebook.rb
@@ -28,31 +28,32 @@ module DoesFacebook
 
     paths["app/helpers"] << "lib/helpers"
   end
-  
+
   # Return the current version of the gem:
   def self.version
     @@version ||= File.open(File.join(File.dirname(__FILE__), "..", "VERSION"), "r").read
   end
-  
+
 end # DoesFacebook
 
 
 module ActionController
   class Base
-    
+
     # Call this method within your controller to enable Facebook actions:
     def self.does_facebook(opts={})
       self.instance_eval do
         include DoesFacebook::ControllerExtensions
         helper :does_facebook
-        prepend_before_filter :parse_signed_request, :validate_signed_request
+        prepend_before_filter :validate_signed_request, opts
+        prepend_before_filter :parse_signed_request, opts
         #prepend_before_filter :sessionify_signed_request unless opts[:session]==false
 
         protected
         attr_reader :fbparams
       end
     end
-    
+
   end
 end # ActionController
 
